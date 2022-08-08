eb02e9aca4c6ca283ba4cb4c455b81c6216988e6
David Prakash Mukhiya <davidmukhiya09@gmail.com>
2022-08-07 23:07:54 -0700

    Removed all previous code. Added a word list file to get a word and generate random word.

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,21 +1,23 @@
 package Hangman;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Hangman {
-    public void startGame(){
-        System.out.println("+----+");
-        System.out.println("");
-        System.out.println("     |");
-        System.out.println("");
-        System.out.println("     |");
-        System.out.println("");
-        System.out.println("     |");
-        System.out.println("");
-        System.out.println("    ====");
-    }
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("C:/Users/sherl/IdeaProjects/Assigments/src/Hangman/words_list.txt"));
+        List<String> words = new ArrayList<>();
+        while(scanner.hasNext()){
+            //System.out.println(scanner.nextLine());
+            words.add(scanner.nextLine());
+        }
 
-    public static void main(String[] args) {
-        Hangman hangman = new Hangman();
-        hangman.startGame();
+        Random random = new Random();
+        String word = words.get(random.nextInt(words.size()+1));
+        System.out.println(word);
     }
-
 }
