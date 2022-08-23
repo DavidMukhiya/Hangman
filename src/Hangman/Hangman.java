597253daac0be985a8f43413ed4ae386c7e2aa0c
David Prakash Mukhiya <davidmukhiya09@gmail.com>
2022-08-22 23:33:03 -0700

    able to create very very  simple hangman

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -10,14 +10,49 @@ import java.util.Scanner;
 public class Hangman {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("C:/Users/sherl/IdeaProjects/Assigments/src/Hangman/words_list.txt"));
+        Scanner keyboard = new Scanner(System.in);
+
         List<String> words = new ArrayList<>();
-        while(scanner.hasNext()){
+        while (scanner.hasNext()) {
             //System.out.println(scanner.nextLine());
             words.add(scanner.nextLine());
         }
 
         Random random = new Random();
-        String word = words.get(random.nextInt(words.size()+1));
+        String word = words.get(random.nextInt(words.size()));
         System.out.println(word);
+
+        List<Character> playerGuesses = new ArrayList<>();
+
+
+        printWordState(word, playerGuesses);
+
+        while(true){
+            getPlayerGuess(keyboard, word, playerGuesses);
+            if(printWordState(word, playerGuesses)){
+                break;
+            }
+        }
+        System.out.println("You win!");
+    }
+
+    private static void getPlayerGuess(Scanner keyboard, String word, List<Character> playerGuesses) {
+        System.out.println("Enter the Character:");
+        playerGuesses.add(keyboard.next().charAt(0));
+
+    }
+
+    private static boolean printWordState(String word, List<Character> playerGuesses) {
+        int correctCount = 0;
+        for(int i = 0; i< word.length(); i++){
+            if(playerGuesses.contains(word.charAt(i))){
+                System.out.print(word.charAt(i));
+                correctCount++;
+            }else{
+                System.out.print("-");
+            }
+        }
+        System.out.println();
+        return (word.length()==correctCount);
     }
 }
